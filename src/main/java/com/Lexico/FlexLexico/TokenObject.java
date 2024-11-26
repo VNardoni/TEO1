9bcfe70d74ce71d090bbebbcd793321f066711fb
SebaJuarez <113382513+SebaJuarez@users.noreply.github.com>
2024-11-26 02:58:00 -0300

    hotfix: Corregido error en la asignación de tipos de los IDs declarados

--- src/main/java/com/Lexico/FlexLexico/TokenObject.java
+++ src/main/java/com/Lexico/FlexLexico/TokenObject.java
@@ -2,4 +2,39 @@ package com.Lexico.FlexLexico;
 
 import java.util.Optional;
 
-public record TokenObject(String name, String value, Optional<String> type) {}
+public class TokenObject {
+
+    private String name;
+    private String value;
+    private Optional<String> type;
+
+    public TokenObject(String name, String value, Optional<String> type) {
+        this.name = name;
+        this.value = value;
+        this.type = type;
+    }
+
+    public String name() {
+        return name;
+    }
+
+    public String value() {
+        return value;
+    }
+
+    public Optional<String> type() {
+        return type;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public void setType(Optional<String> type) {
+        this.type = type;
+    }
+}
